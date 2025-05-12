b7b321bf1a7f3929727e400527092bf9ac0ec2c8
Admin <ecomb26@wgu.edu>
2025-05-11 19:50:10 -0700

    Part H: Write code for the controllers package that includes a REST controller checkout controller class with a post mapping to place orders.

--- src/main/java/com/example/demo/Services/CheckoutService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Services;
-
-import com.example.demo.Services.Purchase;
-import com.example.demo.Services.PurchaseResponse;
-
-public interface CheckoutService {
-    PurchaseResponse placeOrder(Purchase purchase);
-}
