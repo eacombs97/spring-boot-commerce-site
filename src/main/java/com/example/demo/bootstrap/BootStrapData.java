0d640adcd02b77359862ba904cf251b6f71b8d1d
Admin <ecomb26@wgu.edu>
2025-05-11 21:02:20 -0700

    Part I in progress

---
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -0,0 +1,27 @@
+package com.example.demo.bootstrap;
+
+import com.example.demo.dao.CustomerRepository;
+import com.example.demo.dao.DivisionRepository;
+import com.example.demo.entities.Customer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+    private final DivisionRepository divisionRepository;
+
+    public BootStrapData(CustomerRepository customerRepository, DivisionRepository divisionRepository) {
+        this.customerRepository = customerRepository;
+        this.divisionRepository = divisionRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        Set<Customer> customersToAdd = new HashSet<>(customerRepository.findAll());
+    }
+}
